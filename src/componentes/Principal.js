b813a2f49d5d8027b0f8d06dbd7fcc5412b3c973
ing-enmanuel <enmanuelrondon54@gmail.com>
2023-09-30 23:50:52 -0400

    commit-react-tareas

---
+++ src/componentes/Principal.js
@@ -0,0 +1,14 @@
+import React from "react";
+import freedcodecamlogo from "../imagenes/IngenieroEnmanuel.jpg";
+
+function Principal() {
+  return (
+      <div className="freedcodecamp-logo-contenedor">
+        <img src={freedcodecamlogo} className="freedcodecamp-logo" />
+      </div>
+     
+    
+  );
+}
+
+export default Principal;
